e89561118e089c9ff2e3da5b3e724a17fdcd9a0c
TribbianniSun <49214460+TribbianniSun@users.noreply.github.com>
2020-11-10 11:26:23 +0800

    with some more

--- extension/countdown.js
+++ extension/countdown.js
@@ -1,53 +1,76 @@
 
 
-  // this can be anything based on user's design
-  let countMinutes = 60;
-  
-  var Clock = {
-    endSeconds: countMinutes * 60,
-    totalSeconds: 0,
-  
-    start: function () {
-      var self = this;
+// this can be anything based on user's design
+let countMinutes = 60;
+var displaySeconds = countMinutes * 60;
+$("#hours").text(Math.floor(displaySeconds / 3600));
+$("#minutes").text(Math.floor(displaySeconds / 60 % 60));
+$("#seconds").text(parseInt(displaySeconds % 60));
+
+var Clock = {
+  endSeconds: countMinutes * 60,
+  totalSeconds: 0,
+
+
+
+
+  start: function () {
+    var self = this;
+
+
+    this.interval = setInterval(function () {
       
-
-      this.interval = setInterval(function () {
-        
+      if(hasStarted)
         self.totalSeconds += 1;
-        displaySeconds = self.endSeconds - self.totalSeconds;
+      else
+        self.totalSeconds = 0;
+      
+      displaySeconds = self.endSeconds - self.totalSeconds;
 
+      if (displaySeconds >= 0 && hasStarted) {
         $("#hours").text(Math.floor(displaySeconds / 3600));
         $("#minutes").text(Math.floor(displaySeconds / 60 % 60));
         $("#seconds").text(parseInt(displaySeconds % 60));
-      }, 1000);
-    },
-  
-    pause: function () {
-      clearInterval(this.interval);
-      delete this.interval;
-    },
-  
-    resume: function () {
-      if (!this.interval) this.start();
-    }
-  };
-  
-  
-  let isRunning = true;
-  let hasStarted = false;
-  $('#pause').click(function () { 
-    if(isRunning){
-      Clock.pause(); 
-    }
-    else{
-      Clock.resume();
-    }
-    isRunning = !isRunning;
-  
-  });
-  $('#start').click(function () { 
-    if(hasStarted == false){
-      Clock.start(); 
-      hasStarted = true;
-    }
-  });
+      }
+
+      if (displaySeconds <= 0 && hasStarted) {
+        clearInterval(this.interval);
+        delete this.interval;
+        self.totalSeconds = 0;
+        hasStarted = false;
+      }
+    }, 1000);
+  },
+
+ 
+
+  pause: function () {
+    clearInterval(this.interval);
+    delete this.interval;
+  },
+
+  resume: function () {
+    if (!this.interval) this.start();
+  }
+};
+
+
+let isRunning = true;
+let hasStarted = false;
+$('#pause').click(function () {
+  if (isRunning) {
+    Clock.pause();
+  }
+  else {
+    Clock.resume();
+  }
+  isRunning = !isRunning;
+
+});
+$('#start').click(function () {
+  console.log(hasStarted);
+  if (hasStarted == false) {
+    Clock.start();
+    hasStarted = true;
+  }
+});
