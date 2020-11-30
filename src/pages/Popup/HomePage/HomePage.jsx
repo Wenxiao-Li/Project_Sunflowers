40387f8c16c3abf8fe6c98f5eb8deebc330913b1
damonlin9 <59701690+damonlin9@users.noreply.github.com>
2020-11-29 17:42:19 -0800

    Add files via upload

--- src/pages/Popup/HomePage/HomePage.jsx
+++ src/pages/Popup/HomePage/HomePage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import SunflowerBg from '../../../assets/img/IMG_1277.jpg';
+import SunflowerIcon from '../../../assets/img/sunflowerIcon.jpg';
 import DisplaySession from './DisplaySession.jsx';
+//import homeTab from '../../../assets/img/homeTab.png';
+//import settingTab from '../../../assets/img/settingTab.png';
+//import socialTab from '../../../assets/img/socialTab.png';
+//import profileTab from '../../../assets/img/profileTab.png';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div className="Home">
-      <h1>Project Sunflower!</h1>
-      <img src={SunflowerBg} />
+    <div className="Home" >
+      <img src={SunflowerBg} width="360" />
+      <h1> Welcome to our Sunflower Garden! </h1>
       <DisplaySession />
+      <img src={SunflowerIcon} width="50" />
+      <p> You will get One Sunflower per 15 minutes </p>
     </div>
   );
 };
