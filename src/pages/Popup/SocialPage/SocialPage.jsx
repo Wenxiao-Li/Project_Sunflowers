c6bd21f9f817714175c970cd413139b45805702f
dscbg <49822949+dscbg@users.noreply.github.com>
2020-12-03 13:20:03 +0800

    Add files via upload

--- src/pages/Popup/SocialPage/SocialPage.jsx
+++ src/pages/Popup/SocialPage/SocialPage.jsx
@@ -3,7 +3,8 @@ import SunflowerBg from '../../../assets/img/IMG_1277.jpg';
 import Leaderboard from './Leaderboard/Leaderboard.jsx';
 import Friends from './Friends/Friends.jsx';
 import Notifications from './Notifications/Notifications.jsx';
-
+//import { dbRef } from '../../Background/modules/FirebaseConfig';
+import firebase, { db } from '../../Background/modules/firebaseconfig';
 class SocialPage extends Component {
   _isMounted = false;
 
@@ -12,13 +13,78 @@ class SocialPage extends Component {
 
     this.state = {
       displayedComponent: 'Leaderboard',
+
     };
 
     this.showComponent = this.showComponent.bind(this);
+    //this.showComponent('Leaderboard');
   }
 
   componentDidMount() {
     this._isMounted = true;
+    // PLACEHOLDER: array which is used to render the leaderboard(should be replaced by the API Call to backend).
+    var user = this.props.user;
+    var leaderboardRef = db.collection('user');
+    /*
+    if (user) {
+      //let currentFriendsScoreArray = new Array();
+      //console.log(currentFriendsScoreArray);
+      leaderboardRef.doc(user.email).onSnapshot(function (doc) {
+        let currentFriendsScoreArray = new Array();
+        let items = doc.data().friend;
+        //console.log(items);
+        for (var i = 0; i < items.length; i++) {
+          var userID = items[i];
+          leaderboardRef.doc(userID).get().then(function (doc) {
+            if (doc.exists) {
+              let friend = {
+                userName: doc.data().first_name,
+                score: doc.data().user_flower
+              };
+              //console.log(friend);
+              currentFriendsScoreArray.push(friend);
+            } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+            }
+          }).catch(function (error) {
+            console.log("Error getting document:", error);
+
+          });
+        }
+        //console.log('setState')
+        //console.log(currentFriendsScoreArray);
+        this.setState({
+          friendsScore: currentFriendsScoreArray,
+        })
+      }.bind(this))
+    }
+    //console.log(currentFriendsScoreArray);
+    /*
+
+    var currentFriendsScoreArray = [
+      {
+        userName: 'Satyam',
+        score: 12
+      },
+      {
+        userName: 'Yitian',
+        score: 20
+      },
+      {
+        userName: 'HaiHao',
+        score: 30
+      },
+      {
+        userName: 'Fei',
+        score: 40
+      }
+    ];
+*/
+    //Sort the array returned.
+    //currentFriendsScoreArray.sort((a, b) => b.score - a.score);
+
+
   }
 
   componentWillUnmount() {
