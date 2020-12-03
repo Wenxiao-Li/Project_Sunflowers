d669297efe058de342833d4c043884ab534f6604
damonlin9 <59701690+damonlin9@users.noreply.github.com>
2020-12-02 17:37:21 -0800

    Add files via upload

--- src/pages/Popup/HomePage/HomePage.jsx
+++ src/pages/Popup/HomePage/HomePage.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import SunflowerBg from '../../../assets/img/IMG_1277.jpg';
 import SunflowerIcon from '../../../assets/img/sunflowerIcon.jpg';
 import DisplaySession from './DisplaySession.jsx';
-//import homeTab from '../../../assets/img/homeTab.png';
-//import settingTab from '../../../assets/img/settingTab.png';
-//import socialTab from '../../../assets/img/socialTab.png';
-//import profileTab from '../../../assets/img/profileTab.png';
 import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div className="Home" >
 
-      <div className="logo">
-        <img src={SunflowerBg} />
-      </div>
-
       <div className="disSS">
         <div> <DisplaySession /> </div>
       </div>
 
       <div className="sfIcon">
-        <b> 2 </b>
         <img src={SunflowerIcon} width="50" />
       </div>
 
