1c77662dfa70956178e773586db7bfe2f62f6e33
dscbg <49822949+dscbg@users.noreply.github.com>
2020-12-05 11:43:34 +0800

    Add files via upload

--- src/pages/Popup/SocialPage/Leaderboard/Leaderboard.jsx
+++ src/pages/Popup/SocialPage/Leaderboard/Leaderboard.jsx
@@ -1,99 +1,109 @@
 import React, { Component } from 'react';
-import firebase, { db } from '../../../Background/modules/firebaseconfig';
-import SunflowerBg from '../../../../assets/img/IMG_1277.jpg';
-
-class Leaderboard extends Component {
-  _isMounted = false;
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      status: [],
-      friendsScore: [],
-    }
-    var user = this.props.user;
-    var leaderboardRef = db.collection('user');
-    if (user) {
-      //let currentFriendsScoreArray = new Array();
-      //console.log(currentFriendsScoreArray);
-      leaderboardRef.doc(user.email).onSnapshot(function (doc) {
-        let currentFriendsScoreArray = new Array();
-        let items = doc.data().friend;
-        //console.log(items);
-        for (var i = 0; i < items.length; i++) {
-          var userID = items[i];
-          leaderboardRef.doc(userID).get().then(function (doc) {
-            if (doc.exists) {
-              let friend = {
-                userName: doc.data().first_name,
-                score: doc.data().user_flower
-              };
-              //console.log(friend);
-              currentFriendsScoreArray.push(friend);
-            } else {
-              // doc.data() will be undefined in this case
-              console.log("No such document!");
-            }
-          }).catch(function (error) {
-            console.log("Error getting document:", error);
-
-          });
-        }
-        //console.log('setState')
-        //console.log(currentFriendsScoreArray);
-        this.setState({
-          friendsScore: currentFriendsScoreArray,
-        })
-      }.bind(this))
-      console.log(this.state.friendsScore);
-    }
+import likedicon from '../../../../assets/img/liked.png';
+import unlikedicon from '../../../../assets/img/unliked.png';
+import { AuthContext } from '../../../../auth/Auth';
+import { getLeaderboard, unsubscribe } from './getLeaderboard';
+// A leaderboard entry consists of userName, score, and reactions. (Child of Leaderboard)
+class LeaderBoardComponent {
+  constructor(userName, score, reactions, currentUser) {
+    this.userName = userName;
+    this.score = score;
+    this.reactions = reactions;
+    this.currentUser = currentUser;
   }
 
-  componentDidMount() {
-    this._isMounted = true;
-
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.friendsScore != prevProps.friendsScore) {
-      this.setState({
-        status: (this.state.friendsScore)
-      })
+  updateReactions() {
+    console.log(this.reactions);
+    if (this.reactions.indexOf(this.currentUser) === -1) {
+      this.reactions.push(this.currentUser);
+    } else {
+      this.reactions.pop(this.currentUser);
     }
-    //console.log('didupdate');
-    //console.log('did update', this.state.friendsScore);
-  }
-
-  render() {
-
-    console.log('render', this.state.friendsScore)
-
-    var email = 'undefined';
-    var userName = 'undefined';
-    if (this.props.user) {
-      email = this.props.user.email;
-      userName = this.props.user.displayName;
-    }
-
-
-
-
-    return (
-      <div>
-        <h1> Leaderboard</h1>
-        <div> {this.state.friendsScore.map(
-          friend => <div key={friend.userName}>{friend.userName}, Sunflowers = {friend.score}<br /></div>)}</div>
-
-        <span>User Name: {userName}</span>
-        <br />
-        <span>Email: {email}</span>
-      </div>
-    );
   }
 }
+
+const Leaderboard = () => {
+  const [leaderBoardComponents, setLBComponents] = React.useState([]);
+
+  const { user } = React.useContext(AuthContext);
+
+  React.useEffect(() => {
+
+    const setArray = (inputArray) => {
+      var currentFriendsScoreArray = [];
+      currentFriendsScoreArray = inputArray;
+      currentFriendsScoreArray.sort((a, b) => b.score - a.score);
+
+      var newLeaderBoardComponents = [];
+      for (var i = 0; i < currentFriendsScoreArray.length; i++) {
+        var current = currentFriendsScoreArray[i];
+        console.log('print: ', current);
+        var leaderBoardComponent = new LeaderBoardComponent(
+          current.userName,
+          current.score,
+          [],
+          user.displayName
+        );
+        newLeaderBoardComponents.push(leaderBoardComponent);
+      }
+      setLBComponents(newLeaderBoardComponents);
+    };
+    getLeaderboard(user, setArray);
+
+    return unsubscribe;
+  }, []);
+
+  const updateLeaderBoard = (name) => {
+    var nameReactedTo = name;
+    console.log(nameReactedTo);
+    var copy = leaderBoardComponents.slice();
+    for (var i = 0; i < copy.length; i++) {
+      if (copy[i].userName === nameReactedTo) {
+        if (copy[i].reactions.indexOf(copy[i].currentUser) === -1) {
+          console.log('Reaction added for user', nameReactedTo);
+          copy[i].reactions.push(copy[i].currentUser);
+        } else {
+          console.log('Reaction removed for user', nameReactedTo);
+          copy[i].reactions.pop(copy[i].currentUser);
+        }
+      }
+    }
+    setLBComponents(copy);
+  };
+  var email = 'undefined';
+  var userName = 'undefined';
+  if (user) {
+    email = user.email;
+    userName = user.displayName;
+  }
+
+  var leaderboardRendered = leaderBoardComponents.map((friend) => (
+    <div key={friend.userName}>
+      {friend.userName} Sunflowers = {friend.score}
+      <button
+        id={friend.userName}
+        onClick={() => updateLeaderBoard(friend.userName)}
+      >
+        <img
+          src={
+            friend.reactions.indexOf(userName) === -1 ? unlikedicon : likedicon
+          }
+          style={{ width: 20, height: 20 }}
+        ></img>
+      </button>
+      {friend.reactions.length}
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1> Leaderboard</h1>
+      {leaderboardRendered}
+      <span>User Name: {userName}</span>
+      <br />
+      <span>Email: {email}</span>
+    </div>
+  );
+};
 
 export default Leaderboard;
