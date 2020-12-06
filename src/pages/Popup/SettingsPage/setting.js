01be5e196425c6c36436b6c76abfb4fddc126af6
fdai123 <51062221+fdai123@users.noreply.github.com>
2020-12-06 20:27:21 +0800

    Renamed

--- src/pages/Popup/SettingsPage/setting.js
+++ src/pages/Popup/SettingsPage/setting.js
@@ -8,119 +8,148 @@ function changeUsername(firstname, lastname) {
     if (user != null) {
         email = user.email;
     }
-    chrome.runtime.sendMessage({ command: "set_name", firstname: fn, lastname: ln, useremail: email }, (response) => {
-        if (response.message === "success") {
-            //window.location.replace('./popup.html');
-            console.log("Set name");
+    chrome.runtime.sendMessage(
+        { command: 'set_name', firstname: fn, lastname: ln, useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            if (response.message === 'success') {
+                //window.location.replace('./popup.html');
+                console.log('Set name');
+            }
         }
-    });
+    );
 }
 
-function addBlacklist(blacklist) {
-    let bl = blacklist;
+export function addBlocklist(blocklist, callback) {
+    let bl = blocklist;
     var user = firebase.auth().currentUser;
     var email;
     if (user != null) {
         email = user.email;
     }
-    chrome.runtime.sendMessage({ command: "add_blacklist", blacklist: bl, useremail: email }, (response) => {
-        if (response.message === "success") {
-            //window.location.replace('./popup.html');
-            console.log("Add blacklist");
+    chrome.runtime.sendMessage(
+        { command: 'add_blocklist', blocklist: bl, useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            if (response.message === 'success') {
+                //window.location.replace('./popup.html');
+                console.log('Add blocklist');
+                callback();
+            }
         }
-    });
+    );
 }
 
-function addWhitelist(whitelist) {
-    let wl = whitelist;
+export function addAllowlist(allowlist, callback) {
+    let al = allowlist;
+    console.log('al', al);
     var user = firebase.auth().currentUser;
     var email;
     if (user != null) {
         email = user.email;
     }
-    chrome.runtime.sendMessage({ command: "add_whitelist", whitelist: wl, useremail: email }, (response) => {
-        if (response.message === "success") {
-            //window.location.replace('./popup.html');
-            console.log("Add whitelist");
+    chrome.runtime.sendMessage(
+        { command: 'add_allowlist', allowlist: al, useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            if (response.message === 'success') {
+                console.log('Add allowlist');
+                callback();
+            }
         }
-    });
+    );
 }
 
-function deleteWhitelist(whitelist) {
-    let wl = whitelist;
+export function deleteBlocklist(blocklist, callback) {
+    let bl = blocklist;
     var user = firebase.auth().currentUser;
     var email;
     if (user != null) {
         email = user.email;
     }
-    chrome.runtime.sendMessage({ command: "delete_whitelist", whitelist: wl, useremail: email }, (response) => {
-        if (response.message === "success") {
-            //window.location.replace('./popup.html');
-            console.log("Delete whitelist");
+    chrome.runtime.sendMessage(
+        { command: 'delete_blocklist', blocklist: bl, useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            if (response.message === 'success') {
+                //window.location.replace('./popup.html');
+                console.log('Delete blocklist');
+                callback();
+            }
         }
-    });
+    );
 }
 
-function deleteBlacklist(blacklist) {
-    let bl = blacklist;
+export function deleteAllowlist(allowlist, callback) {
+    let al = allowlist;
+    console.log('al', al);
     var user = firebase.auth().currentUser;
     var email;
     if (user != null) {
         email = user.email;
     }
-    chrome.runtime.sendMessage({ command: "delete_blacklist", blacklist: bl, useremail: email }, (response) => {
-        if (response.message === "success") {
-            //window.location.replace('./popup.html');
-            console.log("Delete blacklist");
+    chrome.runtime.sendMessage(
+        { command: 'delete_allowlist', allowlist: al, useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            if (response.message === 'success') {
+                console.log('Delete allowlist');
+                callback();
+            }
         }
-    });
+    );
+}
+
+export function viewWebsitelistHandle(callback) {
+    var user = firebase.auth().currentUser;
+    var email;
+    var blocklist, allowlist;
+    if (user != null) {
+        email = user.email;
+    }
+
+    console.log('I got: ', email);
+    chrome.runtime.sendMessage(
+        { msg: 'view_website', command: 'view_website', useremail: email },
+        (response) => {
+            var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                console.log(lastError.message);
+                return;
+            }
+            // This is bad when you switch to other tabs before callback is called
+            if (response.message === 'success') {
+                blocklist = response.bl;
+                allowlist = response.wl;
+                callback(blocklist, allowlist);
+                console.log('succeeded');
+            } else if (response.message === 'failure') {
+                console.log('failed');
+            }
+        }
+    );
 }
 
 export const changeUsernameHandle = (firstname, lastname) => {
     changeUsername(firstname, lastname);
-};
-
-export const addBlacklistHandle = (blacklist) => {
-    addBlacklist(blacklist);
-};
-
-export const addWhitelistHandle = (whitelist) => {
-    addWhitelist(whitelist);
-};
-
-export const deleteBlacklistHandle = (blacklist) => {
-    deleteBlacklist(blacklist);
-};
-
-export const deleteWhitelistHandle = (whitelist) => {
-    deleteWhitelist(whitelist);
-};
-
-/*export const viewWebsitelistHandle = (callback) => {
-    var user = firebase.auth().currentUser;
-    var email;
-    var blacklist, whitelist;
-    if (user != null) {
-        email = user.email;
-    }
-    chrome.runtime.sendMessage({ command: "view_website", useremail: email }, (response) => {
-        if (response.message === "success") {
-            blacklist = response.bl;
-            whitelist = response.wl;
-            callback(blacklist, whitelist);
-        }
-    })
-};*/
-
-
-// Realtime display
-export const viewWebsitelistHandle = (callback) => {
-    var user = firebase.auth().currentUser;
-    var email;
-    if (user != null) {
-        email = user.email;
-    }
-    db.collection('user').doc(email).onSnapshot(snapshot => {
-        callback(snapshot.data().blacklist, snapshot.data().whitelist)
-    })
 };
