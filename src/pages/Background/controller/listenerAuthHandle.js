aca9ee132aa94a5e03980cc6bc97eb512689bdac
Yitian Wang <yiw031@ucsd.edu>
2020-12-09 21:30:54 +0800

    refactor listenerAuthHandle

--- src/pages/Background/controller/listenerAuthHandle.js
+++ src/pages/Background/controller/listenerAuthHandle.js
@@ -1,33 +1,12 @@
 import firebase from '../modules/firebaseconfig';
-
-import {
-  defaultAllowlist,
-  defaultBlocklist,
-  setUserSnapshot,
-} from './user/user';
+import { guestSetup } from './user/guestSetup';
 
 export const listenerAuthHandle = (listener, callback) => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       listener(user, callback);
     } else {
-      let noLogginSnapShot = {
-        data: function () {
-          return {
-            first_name: 'guest',
-            last_name: 'user',
-            blocklist: defaultBlocklist,
-            allowlist: defaultAllowlist,
-            user_flower: 0,
-            reactions: { '0x1F496': [], '0x1F525': [], '0x1F603': [] },
-            friends: [],
-            friendname: [],
-            friend2: [],
-            friendname2: [],
-          };
-        },
-      };
-      setUserSnapshot(noLogginSnapShot);
+      guestSetup();
     }
   });
 };
